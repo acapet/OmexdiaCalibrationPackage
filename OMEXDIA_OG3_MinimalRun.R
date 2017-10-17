ff4e57322d78be5c7f5044f9a5b26c4eaddce13d
Arthur <arthurcapet@gmail.com>
2017-10-17 12:24:13 +0200

    Testing

--- OMEXDIA_OG3_MinimalRun.R
+++ OMEXDIA_OG3_MinimalRun.R
@@ -16,21 +16,21 @@ require(reshape2)
 source("OMEXDIA_OG3_BasicSetup.R")
 
 # Get observations for a selected station
-source("OMEXDIA_OG3_ObsAtSta.R")
+#source("OMEXDIA_OG3_ObsAtSta.R")
 
 # Convert model outputs to observation-equivalent
-source("OMEXDIA_OG3_DIA2OBS.R")
+#source("OMEXDIA_OG3_DIA2OBS.R")
 
 # Runs the model for a given subset of parameters and returns the model ouptut
 source("OMEXDIA_OG3_OCALL_SS.R")
 
 # Alters the current Porosity profiles and other things according to station info
 # ( It is probably possible to get rid of that one) 
-source("OMEXDIA_OG3_AdaptParsForStation.R")
+#source("OMEXDIA_OG3_AdaptParsForStation.R")
 
 # Utilities for result display 
 source("OMEXDIA_OG3_SimplePlot.R")
-source("OMEXDIA_OG3_ParTablePlot.R")
+#source("OMEXDIA_OG3_ParTablePlot.R")
 #source("OMEXDIA_OG3_FitTablePlot.R")
 #source("OMEXDIA_OG3_FluxTable.R")
 
